f48c6c82ad56070f68ae6c68d98e05a4f5ff4f2c
Ivan-Romo <ivanromontil@gmail.com>
2024-11-10 10:24:57 +0100

    verificación de correo funcionando

--- public/index.php
+++ public/index.php
@@ -29,9 +29,9 @@ $configuration = array(
 );
 
 define('FIXED_SALT', '1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p');
-define('PBKDF2_ALGORITHM', 'sha256'); 
-define('PBKDF2_ITERATIONS', 100000);  
-define('PBKDF2_KEY_LENGTH', 64);      
+define('PBKDF2_ALGORITHM', 'sha256');
+define('PBKDF2_ITERATIONS', 100000);
+define('PBKDF2_KEY_LENGTH', 64);
 
 function create_pbkdf2_hash($password)
 {
@@ -52,10 +52,10 @@ if (isset($_COOKIE['user'])) {
     $configuration['{LOGIN_LOGOUT_TEXT}'] = 'Tancar "sessió"';
     $configuration['{LOGIN_LOGOUT_URL}'] = '/?page=logout';
     $template = 'plantilla_loged';
-} 
+}
 
 if (isset($parameters['page'])) {
-    
+
 
     if ($parameters['page'] == 'register') {
         $template = 'plantilla_register';
@@ -64,14 +64,16 @@ if (isset($parameters['page'])) {
     } else if ($parameters['page'] == 'login') {
         $template = 'plantilla_login';
         $configuration['{LOGIN_USERNAME}'] = '';
-    } else if ($parameters['page'] == 'forgotten') {
+    }else if($parameters['page'] == 'verify'){
+        $template = 'plantilla_verify';
+    } 
+    else if ($parameters['page'] == 'forgotten') {
         $template = 'plantilla_forgotten';
     } else if ($parameters['page'] == 'game') {
         $template = '../public/index';
-    }else if ($parameters['page'] == 'reset_password') {
+    } else if ($parameters['page'] == 'reset_password') {
         $template = 'plantilla_reset_password';
-    }  
-    else if ($parameters['page'] == 'logout') {
+    } else if ($parameters['page'] == 'logout') {
         setcookie('user', '', time() - 3600, "/"); // elimino la cookie si fa logout
         $configuration['{LOGIN_LOGOUT_TEXT}'] = 'Iniciar sessió';
         $configuration['{LOGIN_LOGOUT_URL}'] = '/?page=login';
@@ -79,6 +81,7 @@ if (isset($parameters['page'])) {
         $template = 'plantilla_home';
     }
 } else if (isset($_POST['register'])) {
+    // Verificación de CAPTCHA
     if (!isset($_POST['g-recaptcha-response']) || empty($_POST['g-recaptcha-response'])) {
         $configuration['{FEEDBACK}'] = '<mark>ERROR: Has de completar el CAPTCHA</mark>';
     } else {
@@ -90,8 +93,7 @@ if (isset($parameters['page'])) {
 
         if (!$response_data->success) {
             $configuration['{FEEDBACK}'] = '<mark>ERROR: Verificació CAPTCHA fallida. Torna-ho a intentar.</mark>';
-        } 
-        else {
+        } else {
             $db = new PDO($db_connection);
 
             $sql = 'SELECT COUNT(*) FROM users WHERE user_name = :user_name';
@@ -100,38 +102,75 @@ if (isset($parameters['page'])) {
             $query->execute();
             $user_exists = $query->fetchColumn();
 
-            // Si el nom d'usuari ja existex, mostrem un error
             if ($user_exists > 0) {
+                // Si el nombre de usuario ya existe
                 $configuration['{FEEDBACK}'] = '<mark>ERROR: El nom d\'usuari ja està en ús. Tria un altre.</mark>';
             } else {
+                // Verificación de contraseña y correo electrónico
                 if (strlen($_POST['user_password']) < 8) {
-                    // Si la contraseña te menys de 8 caracters, mostrar un mensaje de error
                     $configuration['{FEEDBACK}'] = '<mark>ERROR: La contrasenya ha de tenir almenys 8 caràcters.</mark>';
                 } elseif (filter_var($_POST['user_name'], FILTER_VALIDATE_EMAIL) === false) {
                     $configuration['{FEEDBACK}'] = '<mark>ERROR: El nom d\'usuari no es un correu electronic.</mark>';
                 } else {
-                    $hashed_password = create_pbkdf2_hash($_POST['user_password']);
+                    // Generar código de verificación y almacenar detalles en la sesión
+                    $verification_code = rand(100000, 999999);
+                    $_SESSION['verification_code'] = $verification_code;
+                    $_SESSION['user_name'] = $_POST['user_name'];
+                    $_SESSION['user_password'] = create_pbkdf2_hash($_POST['user_password']);
 
-                    $sql = 'INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)';
-                    $query = $db->prepare($sql);
-                    $query->bindValue(':user_name', $_POST['user_name']);
-                    $query->bindValue(':user_password', $hashed_password);
-                    if ($query->execute()) {
-                        $configuration['{FEEDBACK}'] = 'Creat el compte <b>' . htmlentities($_POST['user_name']) . '</b>';
-                        $template = 'plantilla_home';
-                    } else {
-                        $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pogut crear el compte <b>"
-                            . htmlentities($_POST['user_name']) . '</b></mark>';
+                    // Enviar correo de verificación
+                    $mail = new PHPMailer(true);
+                    try {
+                        $mail->isSMTP();
+                        $mail->Host = 'smtp.gmail.com';
+                        $mail->SMTPAuth = true;
+                        $mail->Username = 'u1979279@campus.udg.edu';
+                        $mail->Password = 'jejg blkv aubg gsdw';
+                        $mail->SMTPSecure = 'ssl';
+                        $mail->Port = 465;
+
+                        $mail->setFrom('no-reply@tu-dominio.com', 'Nombre de tu App');
+                        $mail->addAddress($_POST['user_name']);
+                        $mail->Subject = "Codi de verificació del compte";
+                        $mail->Body = "Hola,\n\nEl teu codi de verificación es: " . $verification_code . "\n";
+
+                        $mail->send();
+
+                        $configuration['{FEEDBACK}'] = 'Correo de verificación enviado. Revisa tu bandeja de entrada.';
+                        header('Location: /?page=verify'); // Redirige a la página de verificación
+                        exit();
+                    } catch (Exception $e) {
+                        $configuration['{FEEDBACK}'] = '<mark>ERROR: No se pudo enviar el correo de verificación. ' . $mail->ErrorInfo . '</mark>';
                     }
                 }
             }
         }
     }
-
-}else if (isset($_POST['game'])) {
+} else if (isset($_POST['game'])) {
     $template = '../public/index'; // Ruta a la plantilla de juego en public
-}
- else if (isset($_POST['login'])) {
+} else if (isset($_POST['verify'])) {
+    // Verifica si el código ingresado coincide con el de la sesión
+    if ($_POST['verification_code'] == $_SESSION['verification_code']) {
+        // Almacena el usuario en la base de datos
+        $db = new PDO($db_connection);
+        $sql = 'INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)';
+        $query = $db->prepare($sql);
+        $query->bindValue(':user_name', $_SESSION['user_name']);
+        $query->bindValue(':user_password', $_SESSION['user_password']);
+
+        if ($query->execute()) {
+            $configuration['{FEEDBACK}'] = 'Registro completado exitosamente.';
+            unset($_SESSION['verification_code']);
+            unset($_SESSION['user_name']);
+            unset($_SESSION['user_password']);
+            $template = 'plantilla_home';
+        } else {
+            $configuration['{FEEDBACK}'] = '<mark>ERROR: No se pudo completar el registro.</mark>';
+        }
+    } else {
+        $configuration['{FEEDBACK}'] = '<mark>ERROR: Código de verificación incorrecto.</mark>';
+    }
+} else if (isset($_POST['login'])) {
     $db = new PDO($db_connection);
     $sql = 'SELECT * FROM users WHERE user_name = :user_name';
     $query = $db->prepare($sql);
@@ -174,9 +213,9 @@ if (isset($parameters['page'])) {
             $mail->SMTPSecure = 'ssl';
             $mail->Port = 465;
 
-          
+
             $mail->setFrom('no-reply@tu-dominio.com', 'Nombre de tu App');
-            $mail->addAddress($result_row->user_name); 
+            $mail->addAddress($result_row->user_name);
             $mail->Subject = "Restablecer tu contraseña";
             $mail->Body = "Hola,\n\nAquí tienes tu código para restablecer la contraseña: " . $verification_code . "\n\nSi crees que es un error, ignora este correo.";
             $mail->send();
@@ -186,14 +225,14 @@ if (isset($parameters['page'])) {
             $_SESSION['user_name'] = $_POST['user_name'];
             header('Location: /?page=reset_password');
         } catch (Exception $e) {
-  
+
             $configuration['{FEEDBACK}'] = '<mark>ERROR: No se ha podido enviar el correo. Error de PHPMailer: ' . $mail->ErrorInfo . '</mark>';
         }
     } else {
         $configuration['{FEEDBACK}'] = '<mark>ERROR: No existe un usuario con ese correo.</mark>';
 
     }
-} else if (isset($_POST['reset_password'])) {  
+} else if (isset($_POST['reset_password'])) {
 
     if (isset($_POST['user_name'], $_POST['verification_code'], $_POST['new_password'])) {
 
@@ -233,6 +272,6 @@ if (isset($parameters['page'])) {
     }
 }
 // process template and show output
-$html = file_get_contents( $template . '.html', true);
+$html = file_get_contents($template . '.html', true);
 $html = str_replace(array_keys($configuration), array_values($configuration), $html);
 echo $html;
